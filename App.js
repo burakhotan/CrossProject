ed11c6c66b5e4d07602c4377cb8ef35a095307cd
burakhotan <burak.hotan@bahcesehir.edu.tr>
2021-05-05 13:41:36 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import type {Node} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App: () => Node = () => {
+
+  return (
+    <View>
+      <Text>Burak Hotan</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+})
+
+export default App;
